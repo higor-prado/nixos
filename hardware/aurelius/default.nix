df514639e8efef661475b790bacae5d8b8dac8b8
Higor Prado <higorevop@gmail.com>
2026-03-15 11:36:15 -0300

    refactor(perf): make swap policy host-specific

--- hardware/aurelius/default.nix
+++ hardware/aurelius/default.nix
@@ -3,6 +3,7 @@
   imports = [
     ./hardware-configuration.nix
     ./disko.nix
+    ./performance.nix
   ]
   ++ lib.optional (builtins.pathExists ../../private/hosts/aurelius/default.nix) ../../private/hosts/aurelius/default.nix;
 
