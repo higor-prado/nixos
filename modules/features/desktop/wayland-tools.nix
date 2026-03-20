d915a9bb6cc2df432b1a5ddf07dca7f1778c57be
Higor Prado <higorevop@gmail.com>
2026-03-20 12:32:45 -0300

    feat(dendritic): migrate desktop utility surfaces

--- modules/features/desktop/wayland-tools.nix
+++ modules/features/desktop/wayland-tools.nix
@@ -1,5 +1,17 @@
 { ... }:
 {
+  flake.modules.homeManager.wayland-tools =
+    { pkgs, ... }:
+    {
+      home.packages = with pkgs; [
+        wlr-randr
+        waybar
+        swww
+        wl-clipboard
+        libnotify
+      ];
+    };
+
   den.aspects.wayland-tools = {
     provides.to-users.homeManager =
       { pkgs, ... }:
