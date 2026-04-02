53508befe48ef5c47ed6e48a8124c95472dc1fa1
Higor Prado <higorevop@gmail.com>
2026-04-02 19:33:16 -0300

    feat(noctalia): add noctalia feature module

---
+++ modules/features/desktop/noctalia.nix
@@ -0,0 +1,17 @@
+{ inputs, ... }:
+{
+  flake.modules = {
+    nixos.noctalia =
+      { lib, ... }:
+      {
+        services.upower.enable = lib.mkDefault true;
+        services.power-profiles-daemon.enable = lib.mkDefault true;
+      };
+
+    homeManager.noctalia =
+      { pkgs, ... }:
+      {
+        home.packages = [ inputs.noctalia-shell.packages.${pkgs.system}.default ];
+      };
+  };
+}
