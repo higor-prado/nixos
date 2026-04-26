abdcf91192b3a2fca1f093cd490a8dff2c3e1e5c
Higor Prado <higorevop@gmail.com>
2026-04-26 17:49:45 -0300

    chore: update flake.lock and cache configuration

--- modules/features/core/nix-settings-desktop.nix
+++ modules/features/core/nix-settings-desktop.nix
@@ -17,8 +17,8 @@
           "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
           "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
           "zed-industries.cachix.org-1:fgVpvtdF+ssrgP1lB6EusuR3uM6bNcncWduKxri3u6Y="
-          "hyprland.cachix.org-1:7R1/of0Sur8cc4q8oZnCu30CwGR020BgxsRQgQhNj+4="
           "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+          "hyprland.cachix.org-1:7R1/of0Sur8cc4q8oZnCu30CwGR020BgxsRQgQhNj+4="
         ];
       };
     };
