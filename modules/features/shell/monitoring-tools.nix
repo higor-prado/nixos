384e55097411df848507b14c4620d5374bf061aa
Higor Prado <higorevop@gmail.com>
2026-05-01 16:18:22 -0300

    refactor(shell): move monitoring packages to monitoring-tools owner

--- modules/features/shell/monitoring-tools.nix
+++ modules/features/shell/monitoring-tools.nix
@@ -1,8 +1,20 @@
 { ... }:
 {
   flake.modules.homeManager.monitoring-tools =
-    { ... }:
+    { pkgs, ... }:
     {
+      programs.btop = {
+        enable = true;
+        package = pkgs.btop-cuda;
+      };
+      programs.bottom.enable = true;
+
+      home.packages = with pkgs; [
+        htop
+        fastfetch
+        smartmontools
+      ];
+
       xdg.configFile."htop/htoprc".source = builtins.path {
         path = ../../../config/apps/htop/htoprc;
         name = "htoprc";
