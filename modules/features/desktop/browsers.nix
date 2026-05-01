ce7d0f05bfa45a881f5615470f36973a1f73e260
Higor Prado <higorevop@gmail.com>
2026-05-01 16:45:03 -0300

    refactor(desktop): extract browsers from desktop-apps into browsers module

---
+++ modules/features/desktop/browsers.nix
@@ -0,0 +1,41 @@
+{ inputs, ... }:
+{
+  flake.modules.homeManager.browsers =
+    { pkgs, config, ... }:
+    {
+      programs.firefox = {
+        configPath = "${config.xdg.configHome}/mozilla/firefox";
+        enable = true;
+        profiles.default = {
+          id = 0;
+          isDefault = true;
+          path = "y4loqr0b.default";
+          extensions.force = true;
+        };
+        policies = {
+          DisableTelemetry = true;
+        };
+      };
+
+      programs.chromium = {
+        enable = true;
+        commandLineArgs = [
+          "--ozone-platform-hint=auto"
+          "--ozone-platform=wayland"
+          "--enable-features=WaylandWindowDecorations,VaapiVideoDecoder,VaapiVideoEncoder"
+        ];
+      };
+      programs.brave = {
+        enable = true;
+        commandLineArgs = [
+          "--ozone-platform-hint=auto"
+          "--ozone-platform=wayland"
+          "--enable-features=WaylandWindowDecorations,VaapiVideoDecoder,VaapiVideoEncoder"
+        ];
+      };
+
+      home.packages = [
+        inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
+      ];
+    };
+}
--- modules/features/desktop/desktop-apps.nix
+++ modules/features/desktop/desktop-apps.nix
@@ -1,45 +1,13 @@
-{ inputs, ... }:
+{ ... }:
 {
   flake.modules.homeManager.desktop-apps =
-    { pkgs, config, ... }:
+    { pkgs, ... }:
     {
-      programs.firefox = {
-        configPath = "${config.xdg.configHome}/mozilla/firefox";
-        enable = true;
-        profiles.default = {
-          id = 0;
-          isDefault = true;
-          path = "y4loqr0b.default";
-          extensions.force = true;
-        };
-        policies = {
-          DisableTelemetry = true;
-        };
-      };
-
-      programs.chromium = {
-        enable = true;
-        commandLineArgs = [
-          "--ozone-platform-hint=auto"
-          "--ozone-platform=wayland"
-          "--enable-features=WaylandWindowDecorations,VaapiVideoDecoder,VaapiVideoEncoder"
-        ];
-      };
-      programs.brave = {
-        enable = true;
-        commandLineArgs = [
-          "--ozone-platform-hint=auto"
-          "--ozone-platform=wayland"
-          "--enable-features=WaylandWindowDecorations,VaapiVideoDecoder,VaapiVideoEncoder"
-        ];
-      };
-
-      home.packages = [
-        inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
-        pkgs.teams-for-linux
-        pkgs.meld
-        pkgs.obsidian
-        pkgs.super-productivity
+      home.packages = with pkgs; [
+        teams-for-linux
+        meld
+        obsidian
+        super-productivity
       ];
     };
 }
