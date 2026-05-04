7966f27a30372834a8cc9015eec476af1a0065da
Higor Prado <higorevop@gmail.com>
2026-05-04 20:09:15 -0300

    fix(media): remove stremio-linux-shell from desktop media tools

--- modules/features/desktop/media-tools.nix
+++ modules/features/desktop/media-tools.nix
@@ -8,8 +8,7 @@
         pkgs.pamixer
         pkgs.vlc
         pkgs.yt-dlp
-        pkgs.stremio-linux-shell
-        pkgs.playerctl    # media playback control (waybar mpris + media keys)
+        pkgs.playerctl # media playback control (waybar mpris + media keys)
       ];
     };
 }
